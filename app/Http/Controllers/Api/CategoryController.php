8000fc49fcce3714b1071cad13da579a6583c333
PovilasKorop <povilask007@gmail.com>
2025-09-07 21:00:28 +0300

    Lesson 2 - Category show and API resources

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -4,11 +4,22 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Category;
+use App\Http\Resources\CategoryResource;
 
 class CategoryController extends Controller
 {
     public function index()
     {
-        return Category::all();
+        return CategoryResource::collection(Category::all());
+    }
+
+    public function show(Category $category)
+    {
+        return new CategoryResource($category);
+    }
+
+    public function list()
+    {
+        return CategoryResource::collection(Category::all());
     }
 }
